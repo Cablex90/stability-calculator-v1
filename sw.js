f8c6864ba3d08aff098283a632cd5e2e2cc22a91
Cable <cable_x90@gmx.at>
2025-03-29 19:34:43 +0100

    4

--- sw.js
+++ sw.js
@@ -1,10 +1,21 @@
-self.addEventListener("install", function (event) {
-    console.log("Service Worker installiert");
-    event.waitUntil(self.skipWaiting());
-  });
-  
-  self.addEventListener("activate", function (event) {
-    console.log("Service Worker aktiviert");
-    event.waitUntil(self.clients.claim());
-  });
-  
+self.addEventListener("install", event => {
+  event.waitUntil(
+      caches.open("app-cache").then(cache => {
+          return cache.addAll([
+              "/stability-calculator-v1/",
+              "/stability-calculator-v1/index.html",
+              "/stability-calculator-v1/style.css",
+              "/stability-calculator-v1/script.js",
+              "/stability-calculator-v1/icon.png"
+          ]);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+      caches.match(event.request).then(response => {
+          return response || fetch(event.request);
+      })
+  );
+});
